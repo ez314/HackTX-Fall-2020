40bdc0ebc70a944d8d2c1577b9dc4cf7265bea7f
Cory Shen <cory.r.shen@gmail.com>
2020-10-24 14:01:13 -0500

    creating connection to registration

--- frontend/welcome.js
+++ frontend/welcome.js
@@ -1,7 +1,22 @@
-document.getElementById("testText").innerHTML = 'Hello World'
+const lobby = "lobbyWeb1";
 
 document.getElementById("submitUsername").onclick = function(){
 
-    alert("Hello World");
-    alert(userInput);
+    const data = {
+        username: userInput.value,
+        lobby: lobby,
+    }
+
+    const url = "https://us-central1-hacktx-293504.cloudfunctions.net/game/register";
+
+    const response = await fetch(url, {
+        method: 'POST', 
+        mode: 'cors', 
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data) // body data type must match "Content-Type" header
+    });
+
+    console.log(response.json());
 }
