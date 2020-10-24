01de939d469469903a0faa238e53e1915334a401
Eric Zhang <ericz314271@gmail.com>
2020-10-24 10:08:41 -0500

    move and fix register endpoint

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -9,10 +9,8 @@ admin.initializeApp(functions.config().firebase);
 
 //initialize express server
 const app = express();
-const main = express();
 
 //add the path to receive request and set json as bodyParser to process the body 
-main.use('/api/v1', app);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.raw());
@@ -21,39 +19,35 @@ app.use(bodyParser.raw());
 const db = admin.firestore();
 
 //define google cloud function name
-export const webApi = functions.https.onRequest(main);
+export const game = functions.https.onRequest(app);
 
 
 app.post('/register', async (req, res) => {
-    let lobby: string;
-    let username: string;
-
-    try {
-        lobby = req.body['lobby'];
-        username = req.body['username'];
-    } catch (error) {
+    const lobby = req.body['lobby'];
+    const username = req.body['username'];
+    if (!lobby || !username) {
         return res.status(400).json({
             message: 'You are missing the username or lobby, or both',
             success: false
         });
     }
 
-    let doc = await db.collection(`lobbies/${lobby}/users`).doc(username).get();
+    const doc = await db.collection(`lobbies/${lobby}/users`).doc(username).get();
 
     if (doc.exists) {
         return res.status(403).json({
             message: 'That user already exists!',
-            success: false,
+            success: false
         });
     }
     else {
-        const newDoc = await db.collection(`lobbies/${lobby}/users`).doc(username).set({
+        await db.collection(`lobbies/${lobby}/users`).doc(username).set({
             username: username,
             imagesFound: []
         });
         return res.status(201).json({
             message: `${username} has been registered!`,
-            success: true,
+            success: true
         });
     }
 
