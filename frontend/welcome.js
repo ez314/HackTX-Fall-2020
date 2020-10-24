20389651a51a58b6a3d141fcedbd26da7f637b76
Cory Shen <cory.r.shen@gmail.com>
2020-10-24 13:38:19 -0500

    added js files for welcome and lobby

---
+++ frontend/welcome.js
@@ -0,0 +1,7 @@
+document.getElementById("testText").innerHTML = 'Hello World'
+
+document.getElementById("submitUsername").onclick = function(){
+
+    alert("Hello World");
+    alert(userInput);
+}
