b48131eb2e4320b5740af8f63fc13eed18dad62a
rafferli <li.raffer0@gmail.com>
2020-10-24 22:11:48 -0500

    multi rooms

--- frontend/lobbyRoom.js
+++ frontend/lobbyRoom.js
@@ -2,8 +2,9 @@ var url = document.URL;
 
 const urlParams = new URLSearchParams(window.location.search);
 const userName = urlParams.get('user');
+const lobbyName = urlParams.get('lobby');
 console.log(userName);
-var teamnumber = "" 
+var teamnumber = ""
 //create get function for user info from DB
 
 var firebaseConfig = {
@@ -14,119 +15,119 @@ var firebaseConfig = {
     storageBucket: "hacktx-293504.appspot.com",
     messagingSenderId: "827070564073",
     appId: "1:827070564073:web:397e0602af594c0a4dd278"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
-  var dataFromFirestore;
-  var wordList = [];
+var dataFromFirestore;
+var wordList = [];
 
-  var db = firebase.firestore();
+var db = firebase.firestore();
 
-  var team1Ref = db.collection("lobbies/lobbyWeb1/teams").doc("Team_1");
-  var team2Ref = db.collection("lobbies/lobbyWeb1/teams").doc("Team_2");
+var team1Ref = db.collection("lobbies/" + lobbyName + "/teams").doc("Team_1");
+var team2Ref = db.collection("lobbies/" + lobbyName + "/teams").doc("Team_2");
 
-team1Ref.get().then(function(doc) {
+team1Ref.get().then(function (doc) {
     if (doc.exists) {
         console.log("Document data:", doc.data());
-        
+
         dataFromFirestore = doc.data();
         document.getElementById("team1-players").innerHTML = ""
 
-        for (var i = 0; i< dataFromFirestore.players.length; i++){
-        if (dataFromFirestore.players[i] == userName){
-            console.log("you are on TEAM 1")
-            teamnumber="Team_1"
-        }
-        
-        document.getElementById("team1-players").innerHTML = document.getElementById("team1-players").innerHTML + "<br>" + dataFromFirestore.players[i]
+        for (var i = 0; i < dataFromFirestore.players.length; i++) {
+            if (dataFromFirestore.players[i] == userName) {
+                console.log("you are on TEAM 1")
+                teamnumber = "Team_1"
+            }
+
+            document.getElementById("team1-players").innerHTML = document.getElementById("team1-players").innerHTML + "<br>" + dataFromFirestore.players[i]
         }
     } else {
         // doc.data() will be undefined in this case
         console.log("No such document!");
     }
-}).catch(function(error) {
+}).catch(function (error) {
     console.log("Error getting document:", error);
 });
 
-team2Ref.get().then(function(doc) {
+team2Ref.get().then(function (doc) {
     if (doc.exists) {
         console.log("Document data:", doc.data());
-        
+
         dataFromFirestore = doc.data();
         document.getElementById("team2-players").innerHTML = ""
 
-        for (var i = 0; i< dataFromFirestore.players.length; i++){
-            
+        for (var i = 0; i < dataFromFirestore.players.length; i++) {
+
             console.log("RAW" + dataFromFirestore.players[i])
-        if (dataFromFirestore.players[i] == userName){
-            console.log("you are on TEAM 2")
-            teamnumber="Team_2"
-        }
-        document.getElementById("team2-players").innerHTML = document.getElementById("team2-players").innerHTML + "<br>" + dataFromFirestore.players[i]
+            if (dataFromFirestore.players[i] == userName) {
+                console.log("you are on TEAM 2")
+                teamnumber = "Team_2"
+            }
+            document.getElementById("team2-players").innerHTML = document.getElementById("team2-players").innerHTML + "<br>" + dataFromFirestore.players[i]
         }
     } else {
         // doc.data() will be undefined in this case
         console.log("No such document!");
     }
-}).catch(function(error) {
+}).catch(function (error) {
     console.log("Error getting document:", error);
 });
 
-db.collection("lobbies/lobbyWeb1/teams").doc("Team_1")
-.onSnapshot(function(doc) {
-    var source = doc.metadata.hasPendingWrites ? "Local" : "Server";
-    console.log(source, " data: ", doc.data());
-    dataFromFirestore = doc.data();
+db.collection("lobbies/" + lobbyName + "/teams").doc("Team_1")
+    .onSnapshot(function (doc) {
+        var source = doc.metadata.hasPendingWrites ? "Local" : "Server";
+        console.log(source, " data: ", doc.data());
+        dataFromFirestore = doc.data();
         document.getElementById("team1-players").innerHTML = ""
 
-        for (var i = 0; i< dataFromFirestore.players.length; i++){
+        for (var i = 0; i < dataFromFirestore.players.length; i++) {
 
-        document.getElementById("team1-players").innerHTML = document.getElementById("team1-players").innerHTML + "<br>" + dataFromFirestore.players[i]
+            document.getElementById("team1-players").innerHTML = document.getElementById("team1-players").innerHTML + "<br>" + dataFromFirestore.players[i]
         }
-});
+    });
 
-db.collection("lobbies/lobbyWeb1/teams").doc("Team_2")
-.onSnapshot(function(doc) {
-    var source = doc.metadata.hasPendingWrites ? "Local" : "Server";
-    console.log(source, " data: ", doc.data());
-    dataFromFirestore = doc.data();
+db.collection("lobbies/" + lobbyName + "/teams").doc("Team_2")
+    .onSnapshot(function (doc) {
+        var source = doc.metadata.hasPendingWrites ? "Local" : "Server";
+        console.log(source, " data: ", doc.data());
+        dataFromFirestore = doc.data();
         document.getElementById("team2-players").innerHTML = ""
 
-        for (var i = 0; i< dataFromFirestore.players.length; i++){
+        for (var i = 0; i < dataFromFirestore.players.length; i++) {
 
-        document.getElementById("team2-players").innerHTML = document.getElementById("team2-players").innerHTML + "<br>" + dataFromFirestore.players[i]
+            document.getElementById("team2-players").innerHTML = document.getElementById("team2-players").innerHTML + "<br>" + dataFromFirestore.players[i]
         }
-});
+    });
 
-document.getElementById("beginGame").onclick = function() {
+document.getElementById("beginGame").onclick = function () {
 
 
-    db.collection("lobbies").doc("lobbyWeb1").update({
+    db.collection("lobbies").doc(lobbyName).update({
         started: 0
     })
-    .then(function() {
-        console.log("Document successfully written!");
-    })
-    .catch(function(error) {
-        console.error("Error writing document: ", error);
-    });
+        .then(function () {
+            console.log("Document successfully written!");
+        })
+        .catch(function (error) {
+            console.error("Error writing document: ", error);
+        });
 
 
 
 }
 
-db.collection("lobbies").doc("lobbyWeb1")
-.onSnapshot(function(doc) {
-    var source = doc.metadata.hasPendingWrites ? "Local" : "Server";
-    console.log(source, " data: ", doc.data());
-    dataFromFirestore = doc.data();
-    console.log(dataFromFirestore.started)
+db.collection("lobbies").doc(lobbyName)
+    .onSnapshot(function (doc) {
+        var source = doc.metadata.hasPendingWrites ? "Local" : "Server";
+        console.log(source, " data: ", doc.data());
+        dataFromFirestore = doc.data();
+        console.log(dataFromFirestore.started)
 
-    if(dataFromFirestore.started != -1){
-    console.log("GAME STARTING")
-    location.href = "gamePage.html?user=" + userName + "&teamnumber=" + teamnumber;
+        if (dataFromFirestore.started != -1) {
+            console.log("GAME STARTING")
+            location.href = "gamePage.html?user=" + userName + "&teamnumber=" + teamnumber + "&lobby=" + lobbyName;
 
-    }
+        }
 
-});
+    });
--- frontend/welcome.js
+++ frontend/welcome.js
@@ -1,26 +1,26 @@
 const lobby = "lobbyWeb1";
 
-document.getElementById("submitUsername").onclick = async function(){
+document.getElementById("submitUsername").onclick = async function () {
 
     const data = {
         username: userInput.value,
-        lobby: lobby,
+        lobby: userLobbyInput.value,
     }
 
     const url = "https://us-central1-hacktx-293504.cloudfunctions.net/game/register";
 
     fetch(url, {
-        method: 'POST', 
-        mode: 'cors', 
+        method: 'POST',
+        mode: 'cors',
         headers: {
-          'Content-Type': 'application/json'
+            'Content-Type': 'application/json'
         },
         body: JSON.stringify(data) // body data type must match "Content-Type" header
-    }).then(response => response.json()).then( data => {
+    }).then(response => response.json()).then(data => {
         console.log(data);
 
-        if (data.success){
-            location.href = "lobbyRoom.html?user=" + userInput.value;
+        if (data.success) {
+            location.href = "lobbyRoom.html?user=" + userInput.value + "&lobby=" + userLobbyInput.value;
         }
         else {
             document.getElementById("errorMessage").innerHTML = data.message;
